1bc6f49a53a021c0ea0c6c48b486e4ddbe9b3fa3
gadiony <gichanabrian03@gmail.com>
2024-11-04 22:22:48 -0800

    my assignment by brian

---
+++ user-input.py
@@ -0,0 +1,8 @@
+# Get user input
+name = input("Please enter your name: ")
+age = input("Please enter your age: ")
+location = input("Please enter your location: ")
+
+# Create and print personalized message
+message = f"Hello {name}, you are {age} years old and live in {location}."
+print(message)
